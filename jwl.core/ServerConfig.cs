2e675a076309d03b9ba0b51f226b9ca820638d40
Peter Hraško <peter_hrasko@ri-rpc.sk>
2023-03-16 11:29:50 +0100

    🛠 SSL certificate check skip now default to true

--- jwl.core/ServerConfig.cs
+++ jwl.core/ServerConfig.cs
@@ -6,7 +6,7 @@ public class ServerConfig
     public string BaseUrl { get; init; } = @"https://jira.ri-rpc.corp:8080";
     public bool UseProxy { get; init; } = false;
     public int MaxConnectionsPerServer { get; init; } = 8;
-    public bool SkipSslCertificateCheck { get; init; } = false;
+    public bool SkipSslCertificateCheck { get; init; } = true;
     public string JiraUserName { get; init; } = "hrapet";
     public string? JiraUserPassword { get; init; } = null;
 }
