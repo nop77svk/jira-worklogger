4218041d84fb7fdb7f6200d8753796bc19b4b52d
Peter Hraško <peter_hrasko@ri-rpc.sk>
2023-03-17 19:51:03 +0100

    ➕ server config

---
+++ jwl.jira/ServerConfig.cs
@@ -0,0 +1,12 @@
+namespace jwl.jira;
+
+// 2do! config
+public class ServerConfig
+{
+    public string BaseUrl { get; init; } = @"https://jira.ri-rpc.corp:8080";
+    public bool UseProxy { get; init; } = false;
+    public int MaxConnectionsPerServer { get; init; } = 1;
+    public bool SkipSslCertificateCheck { get; init; } = true;
+    public string JiraUserName { get; init; } = "hrapet";
+    public string? JiraUserPassword { get; init; } = @"...";
+}
