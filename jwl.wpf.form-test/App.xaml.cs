151437de780355ac524e3107b182b7811102bdf9
Peter Hraško <peter_hrasko@ri-rpc.sk>
2023-03-22 16:59:07 +0100

    ➕ WPF form design test project

---
+++ jwl.wpf.form-test/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace jwl.wpf.form_test;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
