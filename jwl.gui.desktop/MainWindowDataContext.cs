d3e8bbc527b2ab1a5c56548b25945c0150218905
Peter Hraško <peter_hrasko@ri-rpc.sk>
2023-08-30 12:07:04 +0200

    🔧 ExitApplication command binding at least compiles

---
+++ jwl.gui.desktop/MainWindowDataContext.cs
@@ -0,0 +1,13 @@
+namespace jwl.gui.desktop;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+
+public class MainWindowDataContext
+{
+    public void ExitApplication()
+    {
+        // Application?.Current?.Shutdown();
+    }
+}
