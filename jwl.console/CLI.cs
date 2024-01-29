90bc834dfd732013026cfdcce6be826ad8aabdda
Peter Hraško <phr@whitestein.com>
2024-01-29 22:01:20 +0100

    ➕ configurable activity map

--- jwl.console/CLI.cs
+++ jwl.console/CLI.cs
@@ -22,8 +22,8 @@ public class FillCLI
 
     [Option("server-flavour", HelpText = "Jira server flavour (whether vanilla or with some timesheet plugins)"
         + $"\nJSON config: $.{nameof(core.AppConfig.JiraServer)}.{nameof(jira.ServerConfig.ServerFlavour)}"
-        + $"\nAvailable values: {nameof(jira.JiraServerFlavour.Vanilla)}, {nameof(jira.JiraServerFlavour.TempoTimeSheets)}, {nameof(jira.JiraServerFlavour.ICTimeMockViaJira)}")]
-    public string? ServerClass { get; set; }
+        + $"\nAvailable values: {nameof(jira.JiraServerFlavour.Vanilla)}, {nameof(jira.JiraServerFlavour.TempoTimeSheets)}, {nameof(jira.JiraServerFlavour.ICTime)}")]
+    public string? ServerFlavour { get; set; }
 
     [Option("no-proxy", HelpText = "Turn off proxying the HTTP(S) connections to Jira server"
         + $"\nJSON config: $.{nameof(core.AppConfig.JiraServer)}.{nameof(jira.ServerConfig.UseProxy)} (negated!)")]
@@ -55,7 +55,8 @@ public class FillCLI
             UseVerboseFeedback = UseVerboseFeedback,
             JiraServer = new jira.ServerConfig()
             {
-                ServerFlavour = ServerClass,
+                ServerFlavour = ServerFlavour,
+                ActivityMap = null,
                 BaseUrl = jiraServerSpecification,
                 UseProxy = !NoProxy,
                 MaxConnectionsPerServer = MaxConnectionsPerServer,
--- jwl.jira/ServerConfig.cs
+++ jwl.jira/ServerConfig.cs
@@ -1,10 +1,10 @@
 namespace jwl.jira;
-using System.ComponentModel;
 
 public class ServerConfig
 {
     public string? ServerFlavour { get; init; }
     public JiraServerFlavour ServerFlavourId => ServerApiFactory.DecodeServerClass(ServerFlavour) ?? JiraServerFlavour.Vanilla;
+    public Dictionary<string, string>? ActivityMap { get; init; }
     public string? BaseUrl { get; init; }
     public bool? UseProxy { get; init; }
     public int? MaxConnectionsPerServer { get; init; }
