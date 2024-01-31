3d55019831c868a71cf5ef33cd2f8f0552c328f9
Peter Hraško <phr@whitestein.com>
2024-01-31 09:47:22 +0100

    🛠 MultiTask now handles exceptions and generates AggregateException

--- jwl.infra/MultiTask.cs
+++ jwl.infra/MultiTask.cs
@@ -2,37 +2,52 @@ namespace jwl.infra;
 
 public static class MultiTask
 {
-    public static async Task WhenAll(IEnumerable<Task> tasks, Action<MultiTaskProgressState, Task?> progressFeedback, CancellationToken? cancellationToken = null)
+    public static async Task WhenAll(IEnumerable<Task> tasks, Action<MultiTaskProgressState, Task?>? progressFeedback, CancellationToken? cancellationToken = null)
     {
-        progressFeedback(MultiTaskProgressState.Starting, null);
+        progressFeedback?.Invoke(MultiTaskProgressState.Starting, null);
         HashSet<Task> tasksToExecute = tasks.ToHashSet();
 
-        try
+        List<Exception> errors = new List<Exception>();
+
+        while (tasksToExecute.Any())
         {
-            while (tasksToExecute.Any())
+            Task? taskFinished = null;
+            try
             {
                 cancellationToken?.ThrowIfCancellationRequested();
 
-                Task taskFinished = await Task.WhenAny(tasksToExecute);
-                progressFeedback(MultiTaskProgressState.InProgress, taskFinished);
+                taskFinished = await Task.WhenAny(tasksToExecute);
 
                 if (tasksToExecute.Contains(taskFinished))
+                {
                     tasksToExecute.Remove(taskFinished);
+                    progressFeedback?.Invoke(MultiTaskProgressState.Finished, taskFinished);
+                }
                 else
-                    throw new Exception("Task reported as finished... again!");
+                {
+                    throw new InvalidOperationException("Task reported as finished... again!");
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                progressFeedback?.Invoke(MultiTaskProgressState.Cancelled, ex.Task);
+                errors.Add(ex);
+            }
+            catch (Exception ex)
+            {
+                progressFeedback?.Invoke(MultiTaskProgressState.Error, taskFinished);
+                errors.Add(ex);
             }
         }
-        catch (TaskCanceledException)
+
+        if (errors.Any())
         {
-            progressFeedback(MultiTaskProgressState.Cancelled, null);
-            throw;
-        }
-        catch (Exception)
-        {
-            progressFeedback(MultiTaskProgressState.Error, null);
-            throw;
+            if (errors.All(ex => ex is TaskCanceledException))
+                throw new TaskCanceledException($"All {errors.Count} multitasks have been cancelled");
+            else
+                throw new AggregateException(errors);
         }
 
-        progressFeedback(MultiTaskProgressState.Finished, null);
+        progressFeedback?.Invoke(MultiTaskProgressState.Finished, null);
     }
 }
