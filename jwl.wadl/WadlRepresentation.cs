5b6be769a397d9a5834b7a9e3a32575b8a5c5a7b
pierre@audrey <nop77svk@gmail.com>
2024-02-02 21:56:35 +0100

    🔧 WADL representation media type switched from string to enum

--- jwl.wadl/WadlRepresentation.cs
+++ jwl.wadl/WadlRepresentation.cs
@@ -5,8 +5,23 @@
     [XmlRoot("representation")]
     public class WadlRepresentation
     {
+        public enum MediaTypes
+        {
+            Unknown,
+            [XmlEnum("text/plain")]
+            PlainText,
+            [XmlEnum("application/xml")]
+            Xml,
+            [XmlEnum("application/json")]
+            Json,
+            [XmlEnum("application/atom+xml")]
+            AtomXml,
+            [XmlEnum("application/x-www-form-urlencoded")]
+            WwwFormUrlEncoded
+        }
+
         [XmlAttribute("mediaType")]
-        public string? MediaType { get; set; }
+        public MediaTypes MediaType { get; set; }
 
         [XmlElement("param")]
         public WadlResourceParameter[]? Parameters { get; set; }
