8fe6c8f51ff29f41c96787d62b8f998f77b71ba8
pierre@remote <nop77svk@gmail.com>
2025-09-16 19:53:05 +0200

    🧹 code housekeeping

--- jwl.core/AppConfig.cs
+++ jwl.core/AppConfig.cs
@@ -28,37 +28,3 @@ public class AppConfig
         return result;
     }
 }
-
-public static class AppConfigConversionExtensions
-{
-    public static ServerConfig OverrideWith(this ServerConfig self, ServerConfig? other)
-        => other is null
-        ? self
-        : new ServerConfig()
-        {
-            BaseUrl = other.BaseUrl ?? self.BaseUrl,
-            Flavour = other.Flavour ?? self.Flavour,
-            FlavourOptions = self.FlavourOptions,
-            MaxConnectionsPerServer = other.MaxConnectionsPerServer ?? self.MaxConnectionsPerServer,
-            SkipSslCertificateCheck = other.SkipSslCertificateCheck ?? self.SkipSslCertificateCheck,
-            UseProxy = other.UseProxy ?? self.UseProxy,
-            VanillaJiraFlavourOptions = self.VanillaJiraFlavourOptions
-        };
-
-    public static UserConfig OverrideWith(this UserConfig self, UserConfig? other)
-        => other is null
-        ? self
-        : new UserConfig()
-        {
-            Name = other.Name ?? self.Name,
-            Password = other.Password ?? self.Password
-        };
-
-    public static CsvFormatConfig OverrideWith(this CsvFormatConfig self, CsvFormatConfig? other)
-        => other is null
-        ? self
-        : new CsvFormatConfig()
-        {
-            FieldDelimiter = other.FieldDelimiter ?? self.FieldDelimiter,
-        };
-}
