15a22591ec169c2c76860e1960ea546e33de8b2c
Peter Hraško <16204822+nop77svk@users.noreply.github.com>
2025-11-18 17:54:32 +0100

    dependency on AutoMapper removed (#32)
    * ➖ dependency on AutoMapper removed
    
    * 🔧 fix merge errors
    
    * 🧹 code housekeeping
    
    * 🔧 nuget updates
    
    * 🔧 fix merge errors

--- jwl.core/AppConfig.cs
+++ jwl.core/AppConfig.cs
@@ -1,30 +1,14 @@
 namespace jwl.Core;
 
-using AutoMapper;
+using jwl.Inputs;
+using jwl.Jira;
 
 public class AppConfig
 {
     public bool? UseVerboseFeedback { get; init; }
-    public jwl.Jira.ServerConfig? JiraServer { get; init; }
-    public jwl.Core.UserConfig? User { get; init; }
-    public jwl.Inputs.CsvFormatConfig? CsvOptions { get; init; }
-
-    private static Lazy<MapperConfiguration> overridingMapperConfiguration = new(() => new MapperConfiguration(cfg =>
-    {
-        cfg.CreateMap<AppConfig, AppConfig>()
-            .ForAllMembers(m => m.Condition((src, dest, member) => member != null));
-        cfg.CreateMap<Jira.ServerConfig, Jira.ServerConfig>()
-            .ForAllMembers(m => m.Condition((src, dest, member) => member != null));
-        cfg.CreateMap<Inputs.CsvFormatConfig, Inputs.CsvFormatConfig>()
-            .ForAllMembers(m => m.Condition((src, dest, member) => member != null));
-        cfg.CreateMap<Core.UserConfig, Core.UserConfig>()
-            .ForAllMembers(m => m.Condition((src, dest, member) => member != null));
-
-        cfg.AddGlobalIgnore(nameof(AppConfig.JiraServer.FlavourOptions));
-        cfg.AddGlobalIgnore(nameof(AppConfig.JiraServer.VanillaJiraFlavourOptions));
-    }));
-
-    private static Lazy<IMapper> overridingMapper = new(() => overridingMapperConfiguration.Value.CreateMapper());
+    public ServerConfig? JiraServer { get; init; }
+    public UserConfig? User { get; init; }
+    public CsvFormatConfig? CsvOptions { get; init; }
 
     public AppConfig OverrideWith(AppConfig? other)
     {
@@ -33,7 +17,14 @@ public class AppConfig
             return this;
         }
 
-        AppConfig result = overridingMapper.Value.Map<AppConfig, AppConfig>(other, this);
+        AppConfig result = new AppConfig()
+        {
+            UseVerboseFeedback = other.UseVerboseFeedback ?? UseVerboseFeedback,
+            JiraServer = JiraServer?.OverrideWith(other.JiraServer),
+            User = User?.OverrideWith(other.User),
+            CsvOptions = CsvOptions?.OverrideWith(other.CsvOptions)
+        };
+
         return result;
     }
 }
