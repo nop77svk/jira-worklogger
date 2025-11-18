5c1296a909def112152c885ff2f6ebbfbd4f9ccd
pierre@audrey <nop77svk@gmail.com>
2025-11-18 17:30:54 +0100

    🧹 analyser warnings dealt with

--- jwl.infra/HttpClientExt.cs
+++ jwl.infra/HttpClientExt.cs
@@ -7,6 +7,12 @@ public static class HttpClientExt
 {
     private static readonly XmlSerializerFactory _serializerFactory = new XmlSerializerFactory();
 
+    private static readonly JsonSerializerOptions _defaultJsonSerializerOptions = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        IncludeFields = true
+    };
+
     public static async Task<TResponse> GetAsJsonAsync<TResponse>(this HttpClient self, string uri)
     {
         using Stream response = await self.GetStreamAsync(uri);
@@ -23,13 +29,7 @@ public static class HttpClientExt
 
     public static async Task<TResponse> DeserializeJsonStreamAsync<TResponse>(Stream stream)
     {
-        JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
-        {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            IncludeFields = true
-        };
-
-        TResponse result = await JsonSerializer.DeserializeAsync<TResponse>(stream, jsonSerializerOptions)
+        TResponse result = await JsonSerializer.DeserializeAsync<TResponse>(stream, _defaultJsonSerializerOptions)
             ?? throw new JwlCoreException("JSON deserialization NULL result");
 
         return result;
@@ -42,6 +42,7 @@ public static class HttpClientExt
         {
             object result = xmlSerializer.Deserialize(responseContentStream)
                 ?? throw new JwlCoreException("XML deserialization NULL result");
+
             return result;
         });
 
