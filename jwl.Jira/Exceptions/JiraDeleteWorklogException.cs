a17ae2af4c979389367bc4eb278b7c3ed6b3763c
pierre@remote <nop77svk@gmail.com>
2025-11-19 11:09:07 +0100

    🛠️ refactor delete worklog exception the way the others are made

--- jwl.Jira/Exceptions/JiraDeleteWorklogException.cs
+++ jwl.Jira/Exceptions/JiraDeleteWorklogException.cs
@@ -7,16 +7,29 @@ public class JiraDeleteWorklogException
     public long WorklogId { get; }
 
     public JiraDeleteWorklogException(long issueId, long worklogId)
-        : base($"ID {issueId}", $"Error deleting worklog ID {worklogId} on issue ID {issueId}")
+        : this(issueId, worklogId, (string?)null)
+    {
+    }
+
+    public JiraDeleteWorklogException(long issueId, long worklogId, string? message)
+        : base($"ID {issueId}", FormatMessage(issueId, worklogId, message))
     {
         IssueId = issueId;
         WorklogId = worklogId;
     }
 
     public JiraDeleteWorklogException(long issueId, long worklogId, Exception innerException)
-        : base($"ID {issueId}", $"Error deleting worklog ID {worklogId} on issue ID {issueId}", innerException)
+        : this(issueId, worklogId, (string?)null, innerException)
+    {
+    }
+
+    public JiraDeleteWorklogException(long issueId, long worklogId, string? message, Exception innerException)
+        : base($"ID {issueId}", FormatMessage(issueId, worklogId, message), innerException)
     {
         IssueId = issueId;
         WorklogId = worklogId;
     }
+
+    private static string FormatMessage(long issueId, long worklogId, string? optionalMessage)
+        => DefaultMessageFormatter($"Error deleting worklog ID {worklogId} on issue ID {issueId}", optionalMessage);
 }
